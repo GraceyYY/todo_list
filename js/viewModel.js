d528d15d41ba5c09d8cd90fefd0009fc97175adb
Grace <lunay0325@gmail.com>
2019-01-09 00:30:34 +0800

    Merge the model.js to viewModel.js

--- js/viewModel.js
+++ js/viewModel.js
@@ -1,3 +1,51 @@
+class Todo {
+  constructor(content) {
+    this.state = 'active';
+    this.content = content;
+  }
+}
+
+function initTodo() {
+  localStorage.setItem('active', 0);
+  localStorage.setItem('completed', 0);
+}
+
+function editCounts(name, operation) {
+  let count = parseInt(localStorage.getItem(name));
+  switch (operation) {
+    case 'plus':
+      count += 1;
+      break;
+    case 'minus':
+      count -= 1;
+      break;
+    default:
+      break;
+  }
+  localStorage.setItem(name, count);
+}
+
+function appendTodo(input) {
+  const index = Date.now();
+  let value = JSON.stringify(new Todo(input));
+  localStorage.setItem(index, value);
+  editCounts('active', 'plus');
+}
+
+function hasCompleted(index) {
+  let todo = JSON.parse(localStorage.getItem(index));
+  todo.state = 'completed';
+  localStorage.setItem(index, todo);
+  editCounts('active', 'minus');
+  editCounts('completed', 'plus');
+}
+
+function removeCompleted() {
+  getTodos('completed').forEach(item => {
+    localStorage.removeItem(item.index);
+  });
+}
+
 function getTodoCounts() {
   return parseInt(localStorage.getItem('active')) + parseInt(localStorage.getItem('completed'));
 }
@@ -10,7 +58,8 @@ function getTodos(state) {
     let todo = JSON.parse(localStorage.getItem(key));
     if (todo.state === state) {
       result.unshift({
-        key: todo
+        'index': key,
+        'todo': todo
       });
     }
   }
