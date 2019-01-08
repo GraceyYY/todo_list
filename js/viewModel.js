e456e542d5f533ab6357e3439cefed44b0212eb0
Grace <lunay0325@gmail.com>
2019-01-09 00:11:32 +0800

    Complete the function that can get todos have same states

---
+++ js/viewModel.js
@@ -0,0 +1,16 @@
+function getTodoCounts() {
+  return parseInt(localStorage.getItem('active')) + parseInt(localStorage.getItem('completed'));
+}
+
+function getTodos(state) {
+  let counts = getTodoCounts();
+  let result = [];
+  for (let i = 1; i <= counts; i++) {
+    let key = localStorage.key(i);
+    let todo = JSON.parse(localStorage.getItem(key));
+    if (todo.state === state) {
+      result.unshift(todo);
+    }
+  }
+  return result;
+}
